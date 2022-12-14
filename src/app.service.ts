3b900a2030144a3199f96ad4c726a14977bf0db8
rasujan <sujan.maharjan555@gmail.com>
2022-12-14 21:31:46 +0545

    feat: This is H2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h1> Hello world</h1> <h2> h2 </h2>';
+    return '<h1> Hello world</h1> <h2> h2 This is H2 </h2>';
   }
 }
